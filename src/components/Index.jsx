fa780e8f78e649e1d39e781ae20926db3c2dc905
JhonCamargo07 <jhonalexcamargo07@gmail.com>
2022-10-23 16:37:57 -0500

    Project start

---
+++ src/components/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Index() {
+	return (
+		<>
+			<div className="card">
+				<div className="card-head">
+					<h1>HEADS OR TAILS</h1>
+				</div>
+			</div>
+		</>
+	);
+}
